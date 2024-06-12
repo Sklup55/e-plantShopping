31855cd2442762e4d259fdc2ebbbeff78aeb94d5
Sklup55 <85282385+Sklup55@users.noreply.github.com>
2024-06-12 23:30:29 +0530

    Update CreateSlice.jsx

--- src/CreateSlice.jsx
+++ src/CreateSlice.jsx
@@ -1,38 +1,33 @@
-// venueSlice.js
+// CreateSlice.jsx
 import { createSlice } from "@reduxjs/toolkit";
 
 export const CreateSlice = createSlice({
   name: "cart",
   initialState: {
     items: []
-  
-},
-  reducers: {
-
-    addItem: (state, action) => {
-        const { name, image, cost } = action.payload;
-        const existingItem = state.items.find(item => item.name === name);
-        if (existingItem) {
-          existingItem.quantity++;
-        } else {
-          state.items.push({ name, image, cost, quantity: 1 });
-        }
-      },
-
-      removeItem: (state, action) => {
-        const itemName = action.payload;
-        state.items = state.items.filter(item => item.name !== itemName);
-    },
-
-    updateQuantity: (state, action) => {
-        const { name, quantity } = action.payload;
-        const itemToUpdate = state.items.find(item => item.name === name);
-        if (itemToUpdate) {
-            itemToUpdate.quantity = quantity;
-        }
-    }, 
-
   },
+  reducers: {
+    addItem: (state, action) => {
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
+    },
+    removeItem: (state, action) => {
+      const itemName = action.payload;
+      state.items = state.items.filter(item => item.name !== itemName);
+    },
+    updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
+    }
+  }
 });
 
 export const { addItem, removeItem, updateQuantity } = CreateSlice.actions;
